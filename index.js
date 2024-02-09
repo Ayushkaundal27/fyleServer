2d9d629656dda97e2129b1b5d77167b0801073a8
Ayush Kaundal <140829358+Ayushkaundal27@users.noreply.github.com>
2024-02-10 03:02:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
+const cors = require("cors");
+// Middlewares
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Routes
+app.use("/home", home);
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
